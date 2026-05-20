e28f287abf9a9cc54fcce61b20b3562573a2b1e5
nikhilrautan <nikhilrautan1@gmail.com>
2026-05-20 23:43:01 +0530

    created new folders

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ import express from 'express'
 import{registerUser} from "../controller/user.controller.js";
 
 const router = express.Router();
-router.get("/",registerUser);
+
 router.get("/register",registerUser);
 
 export default router;
