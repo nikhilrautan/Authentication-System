b32778682a83b43fe584739832a7f672685a4692
nikhilrautan <nikhilrautan1@gmail.com>
2026-05-20 15:13:58 +0530

    Remove node_modules

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log
+
+import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config();
+const app = express();
+
+app.use(
+    cors({
+    origin: "http://localhost :3000",
+    Credential: true,
+    methods: ['GET','POST','DELETE','OPTIONS'],
+    allowHeaders :['Content-Type' , 'Authorizationn']
+    })
+);
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+const port = process.env.PORT || 4000;
+
+app.get("/",(req,res) => {
+    res.send("Hello World");
+});
+
+console.log(process.env.PORT);
+app.listen(port, ()=>{
+    console.log(`Example app listening on port ${port}`);
+});
