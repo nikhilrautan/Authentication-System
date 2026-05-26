065f65f5a9257ca4c0c25504a165bb1e513a57b7
nikhilrautan <nikhilrautan1@gmail.com>
2026-05-26 21:50:05 +0530

    UPDATED

--- utils/db.js
+++ utils/db.js
@@ -5,8 +5,9 @@ dotenv.config();
 
 const db = async () => {
   try {
-    console.log("Trying to connect DB...");
     
+    console.log("Trying to connect DB...");
+
     await mongoose.connect(process.env.MONGO_URL);
 
     console.log("MongoDB connected successfully");
