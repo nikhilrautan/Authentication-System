f92f25a07c6ea3fe65c2ce1537260c8250cb071c
nikhilrautan <nikhilrautan1@gmail.com>
2026-05-27 18:27:27 +0530

    UPDATED

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,19 @@
-const registerUser = async(req, res) =>{
-    res.send("registered");
+const registerUser = async (req, res) => {
+
+    const { name, email, password } = req.body;
+
+    res.status(200).json({
+        success: true,
+        message: "All required fields got",
+        user: {
+            name,
+            email,
+            password
+        }
+    });
 };
+
+export { registerUser };
 
 const login = async (req, res)=>{
     res.send("register");
