0dd809eabd4df1bd735deec7c3b3f1f8de4c1410
nikhilrautan <nikhilrautan1@gmail.com>
2026-05-30 09:37:58 +0530

    creating a variable named transporter

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -53,9 +53,9 @@ const registerUser = async (req, res) => {
 
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
-  host: "smtp.example.com",
-  port: 587,
-  secure: false, // use STARTTLS (upgrade connection to TLS after connecting)
+  host: process.env.MAILTRAP_HOST,
+  port: process.env.MAILTRAP_PORT,
+  secure: false, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
