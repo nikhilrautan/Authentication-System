d5b5b6f0d7c976521ef8b4e8ebe3e45259376b86
nikhilrautan <nikhilrautan1@gmail.com>
2026-06-03 22:00:56 +0530

    Updated user.model

--- model/User.model.js
+++ model/User.model.js
@@ -28,11 +28,10 @@ const userSchema = new mongoose.Schema({
  }
 );
 
-userSchema.pre("save",async function(next){
-    if(this.isModified("password")){
-        this.password =await bcrypt.hash(this.password,10)
+userSchema.pre("save", async function () {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 10);
     }
-    next();
 });
 
 const User = mongoose.model("User",userSchema)
