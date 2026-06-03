193ac4cd8143796869b23a74a0b4b6ef37f10738
nikhilrautan <nikhilrautan1@gmail.com>
2026-06-03 21:36:07 +0530

    UPDATED

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -175,11 +175,16 @@ const login = async (req,res)=>{
    res.status(200).json({
     success: true,
     message: "Login successful",
-    
-   })
+    token,
+    user:{
+        id: user._id,
+        name: user.name,
+        role: user.role
+    }
+   });
  } catch (error) {
 
     }
  }
 
-export { registerUser, verifyUser };
+export { registerUser, verifyUser, login};
