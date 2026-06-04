5aa0b1ad4ea3ec55936489c5f78a2cd33cc0b5a4
nikhilrautan <nikhilrautan1@gmail.com>
2026-06-04 19:05:21 +0530

    user controller updated

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -166,7 +166,7 @@ const login = async (req, res) => {
 
         const token = jwt.sign(
             { id: user._id, ROLE: user.role },
-            "shhhhh",
+            ,
             {
                 expiresIn: "24h"
             }
