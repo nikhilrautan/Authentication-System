18fd79b571fa48798f3ab3a8746d9d09b4987b26
nikhilrautan <nikhilrautan1@gmail.com>
2026-06-05 12:05:04 +0530

    Updated auth.middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,3 +1,19 @@
 export const isLoggedIn = async (req,resizeBy,next) =>{
-    
+    try{
+        console.log(req.cookies);
+        let token = req.cookies?.token 
+
+        console.log('Token Found', token ?"YES" : "NO")
+
+        if(!token){
+            console.log("NO token");
+            return res.status(401).json({
+            success: false,
+            message: "Authentication failed"
+            })
+        }
+    }
+    catch(error){
+
+    }
 }
