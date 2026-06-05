782d10756fe1407791987d04325a5713f447190f
nikhilrautan <nikhilrautan1@gmail.com>
2026-06-05 13:13:57 +0530

    Updated user.router

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { registerUser, verifyUser, login } from "../controller/user.controller.js";
+import {
+    registerUser,
+    verifyUser,
+    login,
+    getMe
+} from "../controller/user.controller.js";
+
 import { isLoggedIn } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
@@ -7,6 +13,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.get("/verify/:token", verifyUser);
 router.post("/login", login);
-router.get("/me",isLoggedIn, getMe);
+router.get("/me", isLoggedIn, getMe);
 
 export default router;
