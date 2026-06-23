701b003560355d84e7f6ce79d06211295071ec5e
nikhilrautan <nikhilrautan1@gmail.com>
2026-06-23 23:23:44 +0530

    Update user.router

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -14,7 +14,6 @@ import { isLoggedIn } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-
 router.post("/register", registerUser);
 router.get("/verify/:token", verifyUser);
 router.post("/login", login);
